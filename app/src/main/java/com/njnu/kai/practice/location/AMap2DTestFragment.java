53036c5039337960873e9fd2ab819805385ed959
feiatc <qhk83@qq.com>
2016-11-08 16:38:20 +0800

    test amap uiSettings

--- app/src/main/java/com/njnu/kai/practice/location/AMap2DTestFragment.java
+++ app/src/main/java/com/njnu/kai/practice/location/AMap2DTestFragment.java
@@ -10,12 +10,16 @@ import com.amap.api.location.AMapLocationClient;
 import com.amap.api.location.AMapLocationClientOption;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.maps2d.AMap;
+import com.amap.api.maps2d.AMapOptions;
 import com.amap.api.maps2d.CameraUpdate;
 import com.amap.api.maps2d.CameraUpdateFactory;
+import com.amap.api.maps2d.LocationSource;
 import com.amap.api.maps2d.MapView;
+import com.amap.api.maps2d.UiSettings;
 import com.amap.api.maps2d.model.LatLng;
 import com.njnu.kai.practice.R;
 import com.njnu.kai.support.BaseTestFragment;
+import com.njnu.kai.support.LogUtils;
 import com.njnu.kai.support.ToastUtils;
 import com.njnu.kai.support.ViewUtils;
 
@@ -25,10 +29,13 @@ import com.njnu.kai.support.ViewUtils;
  */
 public class AMap2DTestFragment extends BaseTestFragment implements View.OnClickListener, AMapLocationListener {
 
+    private static final String TAG = "AMap2DTestFragment";
     private MapView mMapView;
     private AMap mMap;
 
     private AMapLocationClient mLocationClient;
+
+    private LocationSource.OnLocationChangedListener mLocationChangedListener;
 
     @Override
     protected View onCreateContentView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
@@ -36,6 +43,33 @@ public class AMap2DTestFragment extends BaseTestFragment implements View.OnClick
         mMapView = (MapView) view.findViewById(R.id.mapview);
         mMapView.onCreate(getArguments());
         mMap = mMapView.getMap();
+
+        UiSettings uiSettings = mMap.getUiSettings();
+//        uiSettings.setScrollGesturesEnabled(false);
+//        uiSettings.setZoomGesturesEnabled(false);
+        uiSettings.setCompassEnabled(true); //指南针、罗盘
+        uiSettings.setZoomControlsEnabled(false); //缩放+-控件
+        uiSettings.setScaleControlsEnabled(true); //比例尺
+        uiSettings.setLogoPosition(AMapOptions.LOGO_POSITION_BOTTOM_RIGHT);
+
+
+        uiSettings.setMyLocationButtonEnabled(true);
+        mMap.setMyLocationEnabled(true);
+        mMap.setLocationSource(new LocationSource() {
+            @Override
+            public void activate(OnLocationChangedListener onLocationChangedListener) {
+                LogUtils.i(TAG, "lookLocation activate");
+                mLocationChangedListener = onLocationChangedListener;
+                mLocationClient.startLocation();
+            }
+
+            @Override
+            public void deactivate() {
+                LogUtils.i(TAG, "lookLocation deactivate");
+                mLocationClient.stopLocation();
+                mLocationChangedListener = null;
+            }
+        });
 
         ViewUtils.bindClickListener(view, this, R.id.basicmap, R.id.rsmap);
 
@@ -109,7 +143,7 @@ public class AMap2DTestFragment extends BaseTestFragment implements View.OnClick
             ToastUtils.showToast("获取定位信息成功: poi=" + location.getPoiName());
             LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
             CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 18);
-            mMap.animateCamera(cameraUpdate, 1500, new AMap.CancelableCallback() {
+            mMap.animateCamera(cameraUpdate, 3000, new AMap.CancelableCallback() {
                 @Override
                 public void onFinish() {
                 }
@@ -120,6 +154,9 @@ public class AMap2DTestFragment extends BaseTestFragment implements View.OnClick
             });
         } else {
             ToastUtils.showToast("获取定位信息失败");
+        }
+        if (mLocationChangedListener != null) {
+            mLocationChangedListener.onLocationChanged(location);
         }
     }
 }
