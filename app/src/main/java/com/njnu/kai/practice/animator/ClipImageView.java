1785ea69d3beb346332080fab6a7f199d973bb96
feiatc <qhk83@qq.com>
2016-12-02 20:29:13 +0800

    test own draw

--- app/src/main/java/com/njnu/kai/practice/animator/ClipImageView.java
+++ app/src/main/java/com/njnu/kai/practice/animator/ClipImageView.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
@@ -23,7 +25,10 @@ public class ClipImageView extends ImageView {
 
     private Paint mEdgePaint;
 
+    private Bitmap mBitmapMask;
     private Bitmap mBitmap;
+
+    private PorterDuffXfermode mXfermode;
 
     public ClipImageView(Context context) {
         super(context);
@@ -44,8 +49,9 @@ public class ClipImageView extends ImageView {
         mEdgePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
         mEdgePaint.setStyle(Paint.Style.FILL_AND_STROKE);
         mEdgePaint.setColor(Color.RED);
-        mEdgePaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
-        mBitmap = ((BitmapDrawable)context.getResources().getDrawable(R.drawable.test_mask)).getBitmap();
+        mXfermode = new PorterDuffXfermode(PorterDuff.Mode.DST_IN);
+        mBitmap = ((BitmapDrawable) context.getResources().getDrawable(R.drawable.img_artist)).getBitmap();
+        mBitmapMask = ((BitmapDrawable) context.getResources().getDrawable(R.drawable.icon_share_sns_friend)).getBitmap();
     }
 
     public void setShowHeight(int height) {
@@ -60,8 +66,31 @@ public class ClipImageView extends ImageView {
             return;
         }
         int saveCount = canvas.saveLayer(0, 0, getRight(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
-        super.onDraw(canvas);
-        canvas.drawBitmap(mBitmap, (getWidth() - mBitmap.getWidth()) / 2, (getHeight() - mBitmap.getHeight()) / 2, mEdgePaint);
+        canvas.drawBitmap(mBitmap, 0, 0, mEdgePaint);
+//        super.onDraw(canvas);
+        mEdgePaint.setXfermode(mXfermode);
+        canvas.drawBitmap(mBitmapMask, (getWidth() - mBitmapMask.getWidth()) / 2, (getHeight() - mBitmapMask.getHeight()) / 2, mEdgePaint);
+        mEdgePaint.setXfermode(null);
         canvas.restoreToCount(saveCount);
+    }
+
+    private void drawOri(Canvas canvas) {
+        Drawable mDrawable = getDrawable();
+        if (mDrawable == null) {
+            return; // couldn't resolve the URI
+        }
+
+        Matrix mDrawMatrix = getImageMatrix();
+        if (mDrawMatrix == null) {
+            mDrawable.draw(canvas);
+        } else {
+            int saveCount = canvas.getSaveCount();
+            canvas.save();
+
+            if (mDrawMatrix != null) {
+                canvas.concat(mDrawMatrix);
+            }
+            mDrawable.draw(canvas);
+        }
     }
 }
