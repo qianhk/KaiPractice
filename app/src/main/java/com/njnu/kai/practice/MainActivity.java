5e0f33799692e7a19ef23201118d4394bbea9e50
feiatc <qhk83@qq.com>
2017-03-29 14:03:01 +0800

    test app links assistant

--- app/src/main/java/com/njnu/kai/practice/MainActivity.java
+++ app/src/main/java/com/njnu/kai/practice/MainActivity.java
@@ -1,8 +1,21 @@
 package com.njnu.kai.practice;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
 import com.njnu.kai.support.base.WrapFragmentActivity;
 
 public class MainActivity extends WrapFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // ATTENTION: This was auto-generated to handle app links.
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
+    }
 
     @Override
     protected Class wrapFragmentClass() {
