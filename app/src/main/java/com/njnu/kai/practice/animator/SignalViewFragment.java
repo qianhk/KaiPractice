0862f3bd8d510fc788e155d87354632b94518f7d
feiatc <qhk83@qq.com>
2017-03-31 16:20:36 +0800

    test modify sFrameDelay, but use vsync

--- app/src/main/java/com/njnu/kai/practice/animator/SignalViewFragment.java
+++ app/src/main/java/com/njnu/kai/practice/animator/SignalViewFragment.java
@@ -11,10 +11,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Interpolator;
 import android.view.animation.LinearInterpolator;
+import android.widget.TextView;
 
 import com.njnu.kai.practice.R;
 import com.njnu.kai.support.BaseTestFragment;
 import com.njnu.kai.support.LogUtils;
+
+import net.vidageek.mirror.dsl.AccessorsController;
+import net.vidageek.mirror.dsl.Mirror;
 
 import java.util.concurrent.TimeUnit;
 
@@ -35,24 +39,25 @@ public class SignalViewFragment extends BaseTestFragment implements View.OnClick
 
     public static final int DURATION = 2000;
     private TestFrameCallback mFrameCallback;
+    private TextView mResultView;
 
     @Override
     protected View onCreateContentView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
-        View signalView = layoutInflater.inflate(R.layout.fragment_signal, viewGroup, false);
-
-        View drawableView = signalView.findViewById(R.id.view_drawable);
+        View rootView = layoutInflater.inflate(R.layout.fragment_signal, viewGroup, false);
+        mResultView = (TextView) rootView.findViewById(R.id.tv_result);
+        View drawableView = rootView.findViewById(R.id.view_drawable);
         mDrawable = (RotateDrawable) layoutInflater.getContext().getResources().getDrawable(R.drawable.xml_footer_refresh);
         drawableView.setBackgroundDrawable(mDrawable);
-//        signalView.findViewById(R.id.btn_add_500).setOnClickListener(this);
+//        rootView.findViewById(R.id.btn_add_500).setOnClickListener(this);
         mObjectAnimator = ObjectAnimator.ofInt(mDrawable, "level", 0, 10000);
         mObjectAnimator.setDuration(DURATION);
         mObjectAnimator.setInterpolator(new LinearInterpolator());
         mObjectAnimator.setRepeatCount(ValueAnimator.INFINITE);
 
-        View drawableView2 = signalView.findViewById(R.id.view_drawable2);
+        View drawableView2 = rootView.findViewById(R.id.view_drawable2);
         mDrawable2 = (RotateDrawable) layoutInflater.getContext().getResources().getDrawable(R.drawable.xml_footer_refresh);
         drawableView2.setBackgroundDrawable(mDrawable2);
-//        signalView.findViewById(R.id.btn_add_500).setOnClickListener(mOnClickListener);
+//        rootView.findViewById(R.id.btn_add_500).setOnClickListener(mOnClickListener);
         mObjectAnimator2 = ObjectAnimator.ofInt(mDrawable2, "level", 0, 10000);
         mObjectAnimator2.setDuration(DURATION);
         mObjectAnimator2.setRepeatCount(ValueAnimator.INFINITE);
@@ -63,8 +68,9 @@ public class SignalViewFragment extends BaseTestFragment implements View.OnClick
             }
         });
 
-        signalView.findViewById(R.id.btn_frame_callback).setOnClickListener(this);
-        return signalView;
+        rootView.findViewById(R.id.btn_frame_callback).setOnClickListener(this);
+        rootView.findViewById(R.id.btn_frame_delay).setOnClickListener(this);
+        return rootView;
     }
 
     @Override
@@ -72,6 +78,14 @@ public class SignalViewFragment extends BaseTestFragment implements View.OnClick
         super.onViewCreated(view, savedInstanceState);
         mObjectAnimator.start();
         mObjectAnimator2.start();
+
+        AccessorsController controller = new Mirror().on(Choreographer.getInstance());
+        Object sFrameDelay = controller.get().field("sFrameDelay");
+        Object USE_VSYNC = controller.get().field("USE_VSYNC");
+        Object USE_FRAME_TIME = controller.get().field("USE_FRAME_TIME");
+
+        String info = "sFrameDelay: " + sFrameDelay + "    USE_VSYNC: " + USE_VSYNC + "   USE_FRAME_TIME: " + USE_FRAME_TIME;
+        mResultView.setText(info);
     }
 
     @Override
@@ -100,10 +114,15 @@ public class SignalViewFragment extends BaseTestFragment implements View.OnClick
             mBeginUptime = SystemClock.uptimeMillis();
             LogUtils.d(TAG, "lookFrame postFrameCallback uptimeMs=%d", mBeginUptime);
             instance.postFrameCallback(mFrameCallback);
+        } else if (viewId == R.id.btn_frame_delay) {
+            AccessorsController controller = new Mirror().on(Choreographer.getInstance());
+            controller.set().field("sFrameDelay").withValue(100L);
+            Long sFrameDelay = (Long) controller.get().field("sFrameDelay");
+            LogUtils.d(TAG, "lookFrame modify  sFrameDelay value=%s", sFrameDelay);
         }
     }
 
-    public static class TestFrameCallback implements Choreographer.FrameCallback {
+    private static class TestFrameCallback implements Choreographer.FrameCallback {
 
         @Override
         public void doFrame(long frameTimeNanos) {
