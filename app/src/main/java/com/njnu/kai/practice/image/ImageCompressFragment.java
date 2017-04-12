17348fd32299d5c5c8738fd2786e786e1c0b347a
feiatc <qhk83@qq.com>
2017-04-12 20:23:10 +0800

    test compressor

--- app/src/main/java/com/njnu/kai/practice/image/ImageCompressFragment.java
+++ app/src/main/java/com/njnu/kai/practice/image/ImageCompressFragment.java
@@ -1,10 +1,13 @@
 package com.njnu.kai.practice.image;
 
+import android.graphics.Bitmap;
+
 import com.njnu.kai.support.BaseTestListFragment;
 import com.njnu.kai.support.TestFunction;
 
 import java.io.File;
 
+import id.zelory.compressor.Compressor;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
@@ -20,7 +23,7 @@ import top.zibin.luban.OnCompressListener;
  */
 public class ImageCompressFragment extends BaseTestListFragment {
 
-    @TestFunction("压缩图片 Use listener")
+    @TestFunction("Luban 压缩图片 Use listener")
     public void onTest01() {
         File file = new File("/sdcard/dcim/camera/IMG_20170412_195631.jpg");
         Luban.get(getContext())
@@ -45,7 +48,7 @@ public class ImageCompressFragment extends BaseTestListFragment {
                 }).launch();
     }
 
-    @TestFunction("压缩图片 use rxJava")
+    @TestFunction("Luban 压缩图片 use rxJava")
     public void onTest02() {
         File file = new File("/sdcard/dcim/camera/IMG_20170412_195631.jpg");
         Luban.get(getContext())
@@ -70,6 +73,51 @@ public class ImageCompressFragment extends BaseTestListFragment {
                     @Override
                     public void call(File file) {
                         appendResult(file.getAbsolutePath());
+                    }
+                });
+    }
+
+    @TestFunction("Compressor 压缩图片  use rxJava")
+    public void onTest03() {
+        File file = new File("/sdcard/dcim/camera/IMG_20170412_195631.jpg");
+        Compressor.getDefault(getContext())
+                .compressToFileAsObservable(file)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<File>() {
+                    @Override
+                    public void call(File file) {
+                        appendResult(file.getAbsolutePath());
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        appendResult(throwable.getMessage());
+                    }
+                });
+    }
+
+    @TestFunction("Compressor 压缩图片  use rxJava max size")
+    public void onTest04() {
+        File file = new File("/sdcard/dcim/camera/IMG_20170412_195631.jpg");
+        new Compressor.Builder(getContext())
+                .setMaxWidth(1200)
+                .setMaxHeight(1500)
+                .setCompressFormat(Bitmap.CompressFormat.WEBP)
+                .setDestinationDirectoryPath("/sdcard/")
+                .build()
+                .compressToFileAsObservable(file)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Action1<File>() {
+                    @Override
+                    public void call(File file) {
+                        appendResult(file.getAbsolutePath());
+                    }
+                }, new Action1<Throwable>() {
+                    @Override
+                    public void call(Throwable throwable) {
+                        appendResult(throwable.getMessage());
                     }
                 });
     }
