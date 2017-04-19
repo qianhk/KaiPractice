0a281845947f09772ca2600a90ac219b5b253aca
feiatc <qhk83@qq.com>
2017-04-19 21:40:10 +0800

    test drawable tint

--- app/src/main/java/com/njnu/kai/practice/image/shadow/ShadowImageFragment.java
+++ app/src/main/java/com/njnu/kai/practice/image/shadow/ShadowImageFragment.java
@@ -1,11 +1,15 @@
 package com.njnu.kai.practice.image.shadow;
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.njnu.kai.practice.R;
+import com.njnu.kai.practice.image.DrawableLess;
 import com.njnu.kai.support.BaseTestFragment;
 
 /**
@@ -14,8 +18,16 @@ import com.njnu.kai.support.BaseTestFragment;
  */
 public class ShadowImageFragment extends BaseTestFragment {
 
+    private View mRootView;
+
     @Override
     protected View onCreateContentView(LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
-        return layoutInflater.inflate(R.layout.fragment_shadow_image, viewGroup, false);
+        mRootView = layoutInflater.inflate(R.layout.fragment_shadow_image, viewGroup, false);
+        ImageView imageView = (ImageView) mRootView.findViewById(R.id.iv_test);
+        imageView.setImageResource(R.drawable.heart);
+        ImageView imageView2 = (ImageView) mRootView.findViewById(R.id.iv_test2);
+        Drawable drawable = getResources().getDrawable(R.drawable.heart);
+        imageView2.setImageDrawable(DrawableLess.$tint(drawable, Color.GREEN));
+        return mRootView;
     }
 }
