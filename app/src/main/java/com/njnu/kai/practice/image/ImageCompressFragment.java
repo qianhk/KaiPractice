6c6213716d94eb914e88000f9579d192a5a85280
qianhk <qhk83@qq.com>
2017-08-26 21:08:45 +0800

    combine luban and compressor

--- app/src/main/java/com/njnu/kai/practice/image/ImageCompressFragment.java
+++ app/src/main/java/com/njnu/kai/practice/image/ImageCompressFragment.java
@@ -1,28 +1,22 @@
 package com.njnu.kai.practice.image;
 
 import android.graphics.Bitmap;
+import android.os.Environment;
 
+import com.njnu.kai.practice.image.compress.Compressor;
 import com.njnu.kai.support.BaseTestListFragment;
 import com.njnu.kai.support.TestFunction;
 
-import org.reactivestreams.Subscription;
-
 import java.io.File;
+import java.io.FileFilter;
+import java.util.Locale;
 
-import id.zelory.compressor.Compressor;
 import io.reactivex.BackpressureStrategy;
 import io.reactivex.Flowable;
 import io.reactivex.FlowableEmitter;
 import io.reactivex.FlowableOnSubscribe;
-import io.reactivex.FlowableSubscriber;
-import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.Consumer;
-import io.reactivex.functions.Function;
 import io.reactivex.schedulers.Schedulers;
-import top.zibin.luban.Luban;
-import top.zibin.luban.OnCompressListener;
 
 /**
  * @author hongkai.qian
@@ -31,51 +25,91 @@ import top.zibin.luban.OnCompressListener;
  */
 public class ImageCompressFragment extends BaseTestListFragment {
 
+    private static final int MAX_FILE_SIZE = 4 * 1024 * 1024;
+
+    private File mOriFile;
+
+    private File prepareOriginalFile() {
+        if (mOriFile == null) {
+            String dcimPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM).getAbsolutePath();
+
+            File file = new File(dcimPath + "/camera/");
+            File[] files = file.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    if (pathname.isDirectory()) {
+                        return false;
+                    }
+                    if (pathname.length() > MAX_FILE_SIZE) {
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            mOriFile = files[0];
+        }
+        return mOriFile;
+    }
+
+    public static String humanReadableByteCount(long bytes, boolean si) {
+        int unit = si ? 1000 : 1024;
+        if (bytes < unit) return bytes + " B";
+        int exp = (int) (Math.log(bytes) / Math.log(unit));
+        String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp - 1) + (si ? "" : "i");
+        return String.format(Locale.getDefault(), "%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+
+    private String makeResultString(File oriFile, File newFile) {
+        return String.format(Locale.getDefault()
+                , "ori=%s oriSize=%s newSize=%s", oriFile.getName()
+                , humanReadableByteCount(oriFile.length(), false)
+                , humanReadableByteCount(newFile.length(), false));
+    }
+
     @TestFunction("Luban 压缩图片 Use listener")
     public void onTest01() {
-        File file = new File("/sdcard/dcim/camera/IMG_20170412_195631.jpg");
-        Luban.with(getContext())
-                .load(file)                     //传人要压缩的图片
-                .setCompressListener(new OnCompressListener() { //设置回调
-
-                    @Override
-                    public void onStart() {
-                        appendResult("onStart");
-                    }
-
-                    @Override
-                    public void onSuccess(File file) {
-                        appendResult(file.getAbsolutePath());
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        appendResult(e.getMessage());
-                    }
-                }).launch();
+//        File file = prepareOriginalFile();
+//        Luban.with(getContext())
+//                .load(file)                     //传人要压缩的图片
+//                .setCompressListener(new OnCompressListener() { //设置回调
+//
+//                    @Override
+//                    public void onStart() {
+//                        appendResult("onStart");
+//                    }
+//
+//                    @Override
+//                    public void onSuccess(File newFile) {
+//                        appendResult(makeResultString(file, newFile));
+//                    }
+//
+//                    @Override
+//                    public void onError(Throwable e) {
+//                        appendResult(e.getMessage());
+//                    }
+//                }).launch();
     }
 
     @TestFunction("Luban 压缩图片 use rxJava")
     public void onTest02() {
-        File file = new File("/sdcard/dcim/camera/IMG_20170412_195631.jpg");
-        Flowable.just(file)
-                .observeOn(Schedulers.io())
-                .map(oriFile -> Luban.with(getActivity()).load(oriFile).get())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(newFile -> appendResult(newFile.getCanonicalPath()), this::appendResult);
+//        File file = prepareOriginalFile();
+//        Flowable.just(file)
+//                .observeOn(Schedulers.io())
+//                .map(oriFile -> Luban.with(getActivity()).load(oriFile).get())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(newFile -> appendResult(makeResultString(file, newFile)), this::appendResult);
     }
 
     @TestFunction("Compressor 压缩图片  use rxJava")
     public void onTest03() {
-        File file = new File("/sdcard/dcim/camera/IMG_20170412_195631.jpg");
+        File file = prepareOriginalFile();
         Flowable.create(new FlowableOnSubscribe<File>() {
             @Override
             public void subscribe(FlowableEmitter<File> e) throws Exception {
                 Compressor compressor = new Compressor(getActivity())
-                        .setMaxWidth(640)
-                        .setMaxHeight(480)
-                        .setQuality(75)
-//                        .setDestinationDirectoryPath(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).getAbsolutePath())
+//                        .setMaxWidth(1500)
+//                        .setMaxHeight(1500)
+                        .setDestinationDirectoryPath(getContext().getExternalCacheDir().getAbsolutePath())
                         .setCompressFormat(Bitmap.CompressFormat.WEBP);
                 e.onNext(compressor.compressToFile(file));
                 e.onComplete();
@@ -83,20 +117,35 @@ public class ImageCompressFragment extends BaseTestListFragment {
         }, BackpressureStrategy.MISSING)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(newFile -> appendResult(file.getAbsolutePath()), this::appendResult);
+                .subscribe(newFile -> appendResult(makeResultString(file, newFile)), this::appendResult);
     }
 
-    @TestFunction("Compressor 压缩图片  use rxJava max size")
+    @TestFunction("Compressor 压缩图片 default size、quality")
     public void onTest04() {
-        File file = new File("/sdcard/dcim/camera/IMG_20170412_195631.jpg");
+        File file = prepareOriginalFile();
         new Compressor(getContext())
-                .setMaxWidth(1200)
-                .setMaxHeight(1500)
-                .setCompressFormat(Bitmap.CompressFormat.WEBP)
-                .setDestinationDirectoryPath("/sdcard/")
+//                .setMaxWidth(1500)
+//                .setMaxHeight(1500)
+//                .setCompressFormat(Bitmap.CompressFormat.JPEG)
+                .setDestinationDirectoryPath(getContext().getExternalCacheDir().getAbsolutePath())
                 .compressToFileAsFlowable(file)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(newFile -> appendResult(file.getAbsolutePath()), this::appendResult);
+                .subscribe(newFile -> appendResult(makeResultString(file, newFile)), this::appendResult);
+    }
+
+    @TestFunction("Compressor 压缩图片  quality 80 ")
+    public void onTest05() {
+        File file = prepareOriginalFile();
+        new Compressor(getContext())
+//                .setMaxWidth(1500)
+//                .setMaxHeight(1500)
+//                .setCompressFormat(Bitmap.CompressFormat.JPEG)
+                .setQuality(80)
+                .setDestinationDirectoryPath(getContext().getExternalCacheDir().getAbsolutePath())
+                .compressToFileAsFlowable(file)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(newFile -> appendResult(makeResultString(file, newFile)), this::appendResult);
     }
 }
---
+++ app/src/main/java/com/njnu/kai/practice/image/compress/ImageUtils.java
@@ -0,0 +1,130 @@
+package com.njnu.kai.practice.image.compress;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created on : June 18, 2016
+ * Author     : zetbaitsu
+ * Name       : Zetra
+ * GitHub     : https://github.com/zetbaitsu
+ */
+class ImageUtils {
+
+    private ImageUtils() {
+
+    }
+
+    static File compressImage(File imageFile, int reqWidth, int reqHeight, Bitmap.CompressFormat compressFormat, int quality, String destinationPath) throws IOException {
+        FileOutputStream fileOutputStream = null;
+        File file = new File(destinationPath).getParentFile();
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+        try {
+            fileOutputStream = new FileOutputStream(destinationPath);
+            // write the compressed bitmap at the destination specified by destinationPath.
+            decodeSampledBitmapFromFile(imageFile, reqWidth, reqHeight).compress(compressFormat, quality, fileOutputStream);
+        } finally {
+            if (fileOutputStream != null) {
+                fileOutputStream.flush();
+                fileOutputStream.close();
+            }
+        }
+
+        return new File(destinationPath);
+    }
+
+    static Bitmap decodeSampledBitmapFromFile(File imageFile, int reqWidth, int reqHeight) throws IOException {
+        // First decode with inJustDecodeBounds=true to check dimensions
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSizeWithLuban(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        Bitmap trueBitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), options);
+
+        //check the rotation of the image and display it properly
+        int angle = 0;
+        ExifInterface exif = new ExifInterface(imageFile.getAbsolutePath());
+        int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION, 0);
+        if (orientation == ExifInterface.ORIENTATION_ROTATE_90) {
+            angle = 90;
+        } else if (orientation == ExifInterface.ORIENTATION_ROTATE_180) {
+            angle = 180;
+        } else if (orientation == ExifInterface.ORIENTATION_ROTATE_270) {
+            angle = 270;
+        }
+        if (angle != 0) {
+            Matrix matrix = new Matrix();
+            matrix.postRotate(angle);
+
+            Bitmap rotateBitmap = Bitmap.createBitmap(trueBitmap, 0, 0, trueBitmap.getWidth(), trueBitmap.getHeight(), matrix, true);
+            if (rotateBitmap != trueBitmap) {
+                trueBitmap.recycle();
+                trueBitmap = rotateBitmap;
+            }
+        }
+        return trueBitmap;
+    }
+
+    private static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+    private static int calculateInSampleSizeWithLuban(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        int srcHeight = options.outHeight;
+        int srcWidth = options.outWidth;
+
+        srcWidth = srcWidth % 2 == 1 ? srcWidth + 1 : srcWidth;
+        srcHeight = srcHeight % 2 == 1 ? srcHeight + 1 : srcHeight;
+
+        int longSide = Math.max(srcWidth, srcHeight);
+        int shortSide = Math.min(srcWidth, srcHeight);
+
+        float scale = ((float) shortSide / longSide);
+        if (scale <= 1 && scale > 0.5625) {
+            if (longSide < 1664) {
+                return 1;
+            } else if (longSide >= 1664 && longSide < 4990) {
+                return 2;
+            } else if (longSide > 4990 && longSide < 10240) {
+                return 4;
+            } else {
+                return longSide / 1280 == 0 ? 1 : longSide / 1280;
+            }
+        } else if (scale <= 0.5625 && scale > 0.5) {
+            return longSide / 1280 == 0 ? 1 : longSide / 1280;
+        } else {
+            return (int) Math.ceil(longSide / (1280.0 / scale));
+        }
+    }
+}
